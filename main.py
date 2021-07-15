703a4580c0b5724d2bb7ea4cbe4a260a2962d4b0
Danny Van Geyte <vangeytedvg@gmail.com>
2021-07-15 17:11:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+    Example from the Kivy Tutorial at : https://kivy.org/doc/stable/tutorials/pong.html
+"""
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongBall(Widget):
+    # velocity of the ball on the x and y axis
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    # referencelist property
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+
+# Important, this is the name of the app, so the .kv file must math the
+# same name, in this case pong.kv
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == '__main__':
+    PongApp().run()
