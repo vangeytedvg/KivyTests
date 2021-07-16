dcdccaca6922dd41637b836fa876aa6966cfa1af
Danny Van Geyte <vangeytedvg@gmail.com>
2021-07-16 11:34:35 +0200

    Testing

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
-"""
-    Example from the Kivy Tutorial at : https://kivy.org/doc/stable/tutorials/pong.html
-"""
-
+import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
-from kivy.vector import Vector
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
-class PongGame(Widget):
-    pass
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGridLayout, self).__init__()
 
-
-class PongBall(Widget):
-    # velocity of the ball on the x and y axis
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    # referencelist property
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-    # TODO: Continue HERE
-
-
-# Important, this is the name of the app, so the .kv file must math the
-# same name, in this case pong.kv
-class PongApp(App):
+class MyApp(App):
     def build(self):
-        return PongGame()
+        return Label(text="Hello World")
 
 
 if __name__ == '__main__':
-    PongApp().run()
+    MyApp().run()
