b8716a08a6d20f9b90a430f04ab33db8e7cb8347
Danny Van Geyte <vangeytedvg@gmail.com>
2021-07-18 11:48:53 +0200

    Video 5 Ok

--- guis2.py
+++ guis2.py
@@ -5,12 +5,22 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 # Video 5/54
 class MyGridLayout(Widget):
+    # These variables refer to the ones in the .kv file
+    # They have no value when the app starts, so we set them
+    # to None to begin with
+    name = ObjectProperty(None)
+    firstname = ObjectProperty(None)
+
     def press(self):
-        pass
+        # This gets the class variables
+        myname = self.name.text
+        myfirstname = self.firstname.text
+        print(f"Hi {myname} {myfirstname}!")
 
 
 class MyApp(App):
