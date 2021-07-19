a08bc877ef1f154ef22276c1bb6e47ede61de71d
Danny Van Geyte <vangeytedvg@gmail.com>
2021-07-19 12:30:42 +0200

    Video 13

--- gui12.py
+++ gui12.py
@@ -2,26 +2,19 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-# Second way to change window background color
 from kivy.core.window import Window
 
+Builder.load_file("video12.kv")
 
 
-
-
-Builder.load_file("video11.kv")
-
-
-# Video 9/54
+# Video 12/54
 class MyBoxLayout(Widget):
     pass
 
 
 class DenkaTechApp(App):
     def build(self):
-        # Second way, comment for first method (kv)
-        # If kv settings remain, they override the code below!
-        Window.clearcolor = (240/255, 141/255, 70/255,1)
+
         return MyBoxLayout()
 
 
